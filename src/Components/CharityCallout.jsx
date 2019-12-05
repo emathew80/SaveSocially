b658c1fb074e369f4b1584e15ca77675b0925e8c
Shane Padgett <shane.padgett@Shane-Padgett-MBP.local>
2019-12-05 10:04:39 -0600

    convert CharityCallout to functional component

---
+++ src/Components/CharityCallout.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { AppContext } from "../AppContext";
+
+function CharityCallout() {
+    let { state, dispatch } = React.useContext(AppContext);
+
+    const charitySearch = (event) => {
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+            targetUrl = `https://projects.propublica.org/nonprofits/api/v2/search.json?q=${event.target.value}`
+        fetch(proxyUrl + targetUrl)
+            .then(blob => blob.json())
+            .then(data => {
+                const prevHashTable = state.allOrgsHashTable
+                const orgHashTable = {}
+                data.organizations.forEach(element => {
+                    orgHashTable[element.name] = element.ein
+                });
+                const combinedHashTable = Object.assign({}, prevHashTable, orgHashTable)
+                dispatch({
+                    type: 'set-allOrgsHashTable',
+                    payload: combinedHashTable,
+                })
+                return data;
+            })
+            .catch(e => {
+                console.log(e);
+                return e;
+            });
+    }
+
+    return (
+        <React.Fragment>
+            <Autocomplete
+                id="free-solo-2-demo"
+                disableClearable
+                options={Object.keys(state.allOrgsHashTable).map(orgName => orgName)}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        label="Search a charity"
+                        margin="normal"
+                        variant="outlined"
+                        fullWidth
+                        onChange={charitySearch}
+                        InputProps={{ ...params.InputProps, type: 'search' }}
+                    />
+                )}
+            />
+        </React.Fragment>
+    )
+}
+export default CharityCallout

07e3e768a783a03dda25d37ab0e4af32cb100896
Shane Padgett <shane.padgett@finastra.com>
2019-12-09 15:26:15 -0600

    add snackbar for transfers

--- src/Components/InternalTransfer.jsx
+++ src/Components/InternalTransfer.jsx
@@ -1,15 +1,52 @@
-import * as React from "react";
-import { Button } from '@material-ui/core';
 import { AppContext } from "../AppContext";
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import { amber, green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { makeStyles } from '@material-ui/core/styles';
+
+const variantIcon = {
+    success: CheckCircleIcon,
+    warning: WarningIcon,
+    error: ErrorIcon,
+    info: InfoIcon,
+};
+
+const useStyles1 = makeStyles(theme => ({
+    success: {
+        backgroundColor: green[600],
+    },
+    error: {
+        backgroundColor: theme.palette.error.dark,
+    },
+    message: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    icon: {
+        marginRight: '12px'
+    }
+}));
 
 export default function InternalTransfer() {
-    let { state, dispatch } = React.useContext(AppContext);
+    const [open, setOpen] = React.useState(false);
+    const [msg, setMsg] = React.useState('');
+    let { state } = React.useContext(AppContext);
     const transfer = async () => {
-    const rawResponse = await fetch(state.proxyUrl + 'https://save-socially-api.herokuapp.com/internal-transfer', {
+        const rawResponse = await fetch(state.proxyUrl + 'https://save-socially-api.herokuapp.com/internal-transfer', {
             method: 'POST',
             headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 fromAccountId: state.fromAccount.accountId,
@@ -20,12 +57,73 @@ export default function InternalTransfer() {
             })
         });
         const content = await rawResponse.json();
-
-        console.log(content);
+        if (!Object.keys(content).length || content.error) {
+            setMsg('Error submitting transfer')
+        } else {
+            setMsg(content.status);
+        }
+        setOpen(true);
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const classes = useStyles1();
+
     return (
-    <Button variant="contained" color="primary" size="large" onClick={transfer}>Click To Transfer Roundup</Button>
+        <React.Fragment>
+            <Button variant="contained" color="primary" size="large" onClick={transfer}>Click To Transfer Roundup</Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={open}
+                autoHideDuration={5000}
+                onClose={handleClose}
+                ContentProps={{
+                    'aria-describedby': 'message-id',
+                }}
+            >
+                {msg.includes('Error') ? (
+                    <MySnackbarContentWrapper
+                        onClose={handleClose}
+                        variant="error"
+                        message={msg}
+                    />
+                ) : (
+                        <MySnackbarContentWrapper
+                            onClose={handleClose}
+                            variant="success"
+                            message={msg}
+                        />
+                    )}
+            </Snackbar>
+        </React.Fragment>
     )
+}
+
+function MySnackbarContentWrapper(props) {
+    const classes = useStyles1();
+    const { className, message, onClose, variant, ...other } = props;
+    const Icon = variantIcon[variant];
+
+    return (
+        <SnackbarContent
+            className={clsx(classes[variant], className)}
+            aria-describedby="client-snackbar"
+            message={
+                <span id="client-snackbar" className={classes.message}>
+                    <Icon className={clsx(classes.icon, classes.iconVariant)} />
+                    {message}
+                </span>
+            }
+        />
+    );
 }
 
